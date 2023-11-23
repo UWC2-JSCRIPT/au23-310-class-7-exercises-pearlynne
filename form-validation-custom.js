89682d53385d0df2127a0859cc04a1dea51e613d
Pearlynne <chongpearlynnelh@gmail.com>
2023-11-22 16:44:26 -0800

    Partial solutions to custom validation

--- form-validation-custom.js
+++ form-validation-custom.js
@@ -1,1 +1,57 @@
 // TODO
+const firstName = document.getElementById("first-name")
+const lastName = document.getElementById("last-name")
+const form = document.getElementById("connect-form")
+const email = document.getElementById("email")
+const select = document.getElementById("contact-kind")
+const os = document.getElementById("operating-system")
+const employees = document.getElementById("num-of-employees")
+
+let valid = false;
+
+// Last and First name length requirement
+const validLength = (input, min) => {
+	if (input.value.trim().length > min) {
+		input.parentElement.classList.remove("invalid");
+		return true;
+	} else {
+		input.parentElement.classList.add("invalid");
+		return false;
+	}
+}
+
+const validateEmail = (emailField) => {
+	const re = /\w+@\w+\.\w+/;
+	if (re.test(emailField.value.trim())) {
+		emailField.parentElement.classList.remove("invalid");
+		return true;
+	} else {
+		emailField.parentElement.classList.add("invalid");
+		return false;
+	}
+}
+
+const handleSelect = (selectElement) => {
+	const selectedValue = selectElement.value;
+	if (selectedValue == "business") {
+		employees.parentElement.classList.remove("hidden");
+		os.parentElement.classList.add("hidden");
+	} else if (selectedValue == "technical") {
+		os.parentElement.classList.remove("hidden");
+		employees.parentElement.classList.add("hidden");
+	}
+}
+
+select.addEventListener("change", () => handleSelect(select))
+
+form.addEventListener("submit", (e) => {
+	handleSelect(select)
+	if (validLength(firstName, 3) && validLength(lastName, 3) && validateEmail(email)
+	) {
+		valid = true;
+	} else {
+		valid = false;
+		console.log("Bad Input")
+	}
+	e.preventDefault()
+}) 
